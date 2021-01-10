92edf8800c878934d244634b58181dd6c1c514f7
coder-oyz <1583807155@qq.com>
2021-01-10 11:13:08 +0800

    广播最佳实践，实现发送广播。强制退出

--- app/src/main/java/com/oyz/broadcast/bestpractice/BaseActivity.kt
+++ app/src/main/java/com/oyz/broadcast/bestpractice/BaseActivity.kt
@@ -1,18 +1,56 @@
 package com.oyz.broadcast.bestpractice
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-
+//在BaseActivity动态注册一个BroadcastRecevier是所有Activity都有一个
 open class BaseActivity :AppCompatActivity() {
+
+    lateinit var receiver: ForceOfflineReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ActivityCollector.addActivity(this)
     }
 
+    override fun onResume() {
+        super.onResume()
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("com.oyz.broadcast.bestpractice.FORCE_OFFLINE")
+        receiver = ForceOfflineReceiver()
+        registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(receiver)
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         ActivityCollector.removeActivity(this)
+    }
+
+    inner class ForceOfflineReceiver : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            AlertDialog.Builder(context).apply {
+                setTitle("Warning")
+                setMessage("You are forced to be offline. Please try to login again.")
+                setCancelable(false)
+                setPositiveButton("OK") { _, _ ->
+                    ActivityCollector.finishAll() // 销毁所有Activity
+                    val i = Intent(context, LoginActivity::class.java)
+                    context.startActivity(i) // 重新启动LoginActivity
+                }
+                show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/oyz/broadcast/bestpractice/MainActivity.kt
+++ app/src/main/java/com/oyz/broadcast/bestpractice/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.oyz.broadcast.bestpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //发送下线广播
+        forceOffline.setOnClickListener {
+            Toast.makeText(this,"click",Toast.LENGTH_SHORT).show()
+            val intent = Intent("com.oyz.broadcast.bestpractice.FORCE_OFFLINE")
+            sendBroadcast(intent)
+        }
     }
 }
