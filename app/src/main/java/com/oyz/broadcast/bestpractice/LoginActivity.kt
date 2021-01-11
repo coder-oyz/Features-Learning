febcb9524e33b2c34e78ce375dc8c241b4ae5b8c
coder-oyz <1583807155@qq.com>
2021-01-11 11:05:21 +0800

    记住密码

--- app/src/main/java/com/oyz/broadcast/bestpractice/LoginActivity.kt
+++ app/src/main/java/com/oyz/broadcast/bestpractice/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.oyz.broadcast.bestpractice
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -11,11 +13,35 @@ class LoginActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val prefs = getPreferences( Context.MODE_PRIVATE)
+        val isRemember = prefs.getBoolean("remember_password", false)
+
+        if(isRemember){
+            //将账号密码设置到文本框中
+            val account = prefs.getString("account", "")
+            val password = prefs.getString("password", "")
+            accountEdit.setText(account)
+            passwordEdit.setText(password)
+            rememberPass.isChecked = true
+        }
+
+
+
         login.setOnClickListener {
             val account = accountEdit.text.toString()
             val password = passwordEdit.text.toString()
             // 如果账号是admin且密码是123456，就认为登录成功
             if (account == "admin" && password == "123456") {
+                val editor = prefs.edit()
+                if(rememberPass.isChecked) { //检查复选框是否被选中
+                    editor.putBoolean("remember_password", true)
+                    editor.putString("account", account)
+                    editor.putString("password", password)
+                }else {
+                    editor.clear()
+                }
+                editor.apply()
+
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
