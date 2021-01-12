580c0aa1619e73971df3adf978634b0af1c05653
coder-oyz <1583807155@qq.com>
2021-01-12 11:08:33 +0800

    数据库学习项目。数据库升级写法优化

--- app/src/main/java/com/oyz/databasetest/MyDatabaseHelper.kt
+++ app/src/main/java/com/oyz/databasetest/MyDatabaseHelper.kt
@@ -13,6 +13,13 @@ class MyDatabaseHelper(val context: Context, name: String, version: Int): SQLite
             "price real," +
             "pages integer," +
             "name text)"
+    private val createBook2 = "create table Book (" +
+            "id integer primary key autoincrement," +
+            "author text," +
+            "price real," +
+            "pages integer," +
+            "name text," +
+            "category_id integer)"
 
     private val createCategory = "create table Category (" +
             "id integer primary key autoincrement," +
@@ -24,15 +31,26 @@ class MyDatabaseHelper(val context: Context, name: String, version: Int): SQLite
     override fun onCreate(db: SQLiteDatabase) {
         //执行sql
         db.execSQL(createBook)
-        db.execSQL(createCategory)
+        //db.execSQL(createCategory)
         Toast.makeText(context, "Create succeeded", Toast.LENGTH_SHORT).show()
     }
     //onUpgrade()方法是用于对数据库进行升级的，它在整个数据库的管理工作当中起着非常重要的作用。
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+    /*override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         db.execSQL("drop table if exists Book")
         db.execSQL("drop table if exists Category")
         onCreate(db)
 
+    }*/
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        if(oldVersion<=1){
+            //第一天需求，加一个表
+            db.execSQL(createCategory)
+        }
+        if(oldVersion<=2){
+            //第二天需求，改一个表结构
+            db.execSQL("alter table Book add column category_id integer")
+        }
     }
 
 
