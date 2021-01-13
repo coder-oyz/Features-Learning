c6ef3a2c739be339f2408a3cf968048e765f00a4
coder-oyz <1583807155@qq.com>
2021-01-13 12:56:10 +0800

    获取拨号权限后，进行拨号

--- app/src/main/java/com/oyz/runtimepermissiontest/MainActivity.kt
+++ app/src/main/java/com/oyz/runtimepermissiontest/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.oyz.runtimepermissiontest
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //点击事件，出发拨打电话的逻辑
+        //必须进行运行时权限处理，否则android以上得就不能执行
+        makeCall.setOnClickListener {
+            //判断是否拥有权限
+            if (ContextCompat.checkSelfPermission(this,  Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), 1)
+            } else {
+                call()
+            }
+        }
+
+    }
+
+    //申请权限
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+
     }
 }
