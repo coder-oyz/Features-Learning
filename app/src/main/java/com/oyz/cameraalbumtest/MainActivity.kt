eee056c063dbe347b2fe44829aae1023ea7d5e05
coder-oyz <1583807155@qq.com>
2021-01-17 10:28:41 +0800

    摄像头调用,读取相册

--- app/src/main/java/com/oyz/cameraalbumtest/MainActivity.kt
+++ app/src/main/java/com/oyz/cameraalbumtest/MainActivity.kt
@@ -1,11 +1,85 @@
 package com.oyz.cameraalbumtest
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+    val takePhoto = 1
+    val fromAlbum = 2
+    lateinit var imageUri: Uri
+    lateinit var outputImage: File
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //读取相册
+        fromAlbumBtn.setOnClickListener {
+            //打开文件选择器
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            //指定只显示图片
+            intent.type = "image/*"
+            startActivityForResult(intent, fromAlbum)
+
+        }
+
+        //拍照
+        takePhotoBtn.setOnClickListener {
+            //创建File对象，用于存储拍好后的图片
+            outputImage = File(externalCacheDir, "output_image.jpg")
+            if (outputImage.exists()) {
+                outputImage.delete()
+            }
+            outputImage.createNewFile()
+            imageUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                FileProvider.getUriForFile(this, "com.oyz.cameraalbumtest.fileprovider", outputImage)
+            } else {
+                Uri.fromFile(outputImage)
+            }
+            //启动相机程序
+            val intent = Intent("android.media.action.IMAGE_CAPTURE")
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            startActivityForResult(intent, takePhoto)
+        }
+
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            takePhoto -> {
+                if (resultCode == Activity.RESULT_OK) {val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri))
+                    imageView.setImageBitmap( bitmap)
+                }
+            }
+
+            fromAlbum -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    data.data?.let { uri ->
+                        val bitmap = getBitmapFromUri(uri)
+                        imageView.setImageBitmap(bitmap)
+                    }
+                }
+            }
+
+        }
+    }
+
+    private fun getBitmapFromUri(uri: Uri) = contentResolver
+        .openFileDescriptor(uri, "r")?.use {
+        BitmapFactory.decodeFileDescriptor(it.fileDescriptor)
+    }
+
 }
