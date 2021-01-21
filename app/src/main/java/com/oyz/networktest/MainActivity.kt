fc9a7447ece02e31dd4b5b029867bbe3858ec9b2
coder-oyz <1583807155@qq.com>
2021-01-21 10:20:25 +0800

    Pull解析方式

--- app/src/main/java/com/oyz/networktest/MainActivity.kt
+++ app/src/main/java/com/oyz/networktest/MainActivity.kt
@@ -2,12 +2,21 @@ package com.oyz.networktest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
+import org.xml.sax.InputSource
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserFactory
 import java.io.BufferedReader
 import java.io.DataOutputStream
 import java.io.InputStreamReader
+import java.io.StringReader
 import java.net.HttpURLConnection
 import java.net.URL
+import javax.xml.parsers.SAXParserFactory
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +25,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         sendRequestBtn.setOnClickListener {
-            sendRequestWithHttpURLConnection()
-            //sendRequestWithOkHttp()
+            //sendRequestWithHttpURLConnection()
+            sendRequestWithOkHttp()
         }
     }
 
@@ -63,26 +72,120 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /*private fun sendRequestWithOkHttp() {
+    /**
+     * 使用post
+     *  val requestBody = FormBody.Builder()
+            .add("username","admin")
+            .add("password","123456")
+            .build()
+
+     *  val request = Request.Builder()
+            .url("https://www.baidu.com")
+            .post(requestBody)
+            .build()
+     */
+    private fun sendRequestWithOkHttp() {
         thread {
             try {
                 val client = OkHttpClient()
                 val request = Request.Builder()
                         // 指定访问的服务器地址是电脑本机
-//                    .url("http://10.0.2.2/get_data.xml")
-                        .url("http://10.0.2.2/get_data.json")
+                       // .url("https://www.baidu.com")
+                    .url("http://10.0.2.2/get_data.xml")
+//                        .url("http://10.0.2.2/get_data.json")
                         .build()
                 val response = client.newCall(request).execute()
                 val responseData = response.body?.string()
                 if (responseData != null) {
-//                    parseXMLWithPull(responseData)
+                    //showResponse(responseData)
+
+                        //按照Pull解析方式
+                    parseXMLWithPull(responseData)
 //                    parseXMLWithSAX(responseData)
-//                    parseJSONWithJSONObject(responseData)
-                    parseJSONWithGSON(responseData)
+ //                   parseJSONWithJSONObject(responseData)
+                    //parseJSONWithGSON(responseData)
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
             }
+        }
+    }
+
+    private fun parseXMLWithPull(xmlData: String) {
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            val xmlPullParser = factory.newPullParser()
+            xmlPullParser.setInput(StringReader(xmlData))
+            var eventType = xmlPullParser.eventType
+            var id = ""
+            var name = ""
+            var version = ""
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                val nodeName = xmlPullParser.name
+                when (eventType) {
+                    // 开始解析某个节点
+                    XmlPullParser.START_TAG -> {
+                        when (nodeName) {
+                            "id" -> id = xmlPullParser.nextText()
+                            "name" -> name = xmlPullParser.nextText()
+                            "version" -> version = xmlPullParser.nextText()
+                        }
+                    }
+                    // 完成解析某个节点
+                    XmlPullParser.END_TAG -> {
+                        if ("app" == nodeName) {
+                            Log.d("MainActivity", "id is $id")
+                            Log.d("MainActivity", "name is $name")
+                            Log.d("MainActivity", "version is $version")
+                        }
+                    }
+                }
+                eventType = xmlPullParser.next()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    /*private fun parseXMLWithSAX(xmlData: String) {
+        try {
+            val factory = SAXParserFactory.newInstance()
+            val xmlReader = factory.newSAXParser().getXMLReader()
+            val handler = ContentHandler()
+            // 将ContentHandler的实例设置到XMLReader中
+            xmlReader.contentHandler = handler
+            // 开始执行解析
+            xmlReader.parse(InputSource(StringReader(xmlData)))
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun parseJSONWithJSONObject(jsonData: String) {
+        try {
+            val jsonArray = JSONArray(jsonData)
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray.getJSONObject(i)
+                val id = jsonObject.getString("id")
+                val name = jsonObject.getString("name")
+                val version = jsonObject.getString("version")
+                Log.d("MainActivity", "id is $id")
+                Log.d("MainActivity", "name is $name")
+                Log.d("MainActivity", "version is $version")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun parseJSONWithGSON(jsonData: String) {
+        val gson = Gson()
+        val typeOf = object : TypeToken<List<App>>() {}.type
+        val appList = gson.fromJson<List<App>>(jsonData, typeOf)
+        for (app in appList) {
+            Log.d("MainActivity", "id is ${app.id}")
+            Log.d("MainActivity", "name is ${app.name}")
+            Log.d("MainActivity", "version is ${app.version}")
         }
     }*/
 }
