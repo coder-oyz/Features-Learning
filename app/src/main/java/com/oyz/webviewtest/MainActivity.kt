ab23d0825803ee6af23a7d6477e6f65e173f8997
coder-oyz <1583807155@qq.com>
2021-01-21 09:26:15 +0800

    实现访问百度

--- app/src/main/java/com/oyz/webviewtest/MainActivity.kt
+++ app/src/main/java/com/oyz/webviewtest/MainActivity.kt
@@ -2,10 +2,18 @@ package com.oyz.webviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //支持js
+        webView.settings.setJavaScriptEnabled(true)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.baidu.com")
+
     }
 }
