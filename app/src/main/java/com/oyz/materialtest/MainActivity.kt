60a7714efef91c41caf7ef63f9ea94ec9ab2dcbe
coder-oyz <1583807155@qq.com>
2021-01-23 09:39:47 +0800

    滑动窗口

--- app/src/main/java/com/oyz/materialtest/MainActivity.kt
+++ app/src/main/java/com/oyz/materialtest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.view.GravityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,11 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
-
+        //增加导航按钮
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
 
     }
 
@@ -26,6 +31,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
+            android.R.id.home -> drawerLayout.openDrawer(GravityCompat.START)  //导航按钮   开启滑动窗口
+
             R.id.backup -> Toast.makeText(this, "You clicked BackUp", Toast.LENGTH_SHORT).show()
 
             R.id.delete -> Toast.makeText(this, "You clicked Delete", Toast.LENGTH_SHORT).show()
