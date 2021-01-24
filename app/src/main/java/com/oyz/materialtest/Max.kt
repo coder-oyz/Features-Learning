1bbee64ae2cc343e212e8fdbc4e7d171dfc06f31
coder-oyz <1583807155@qq.com>
2021-01-24 10:20:00 +0800

    编写好用的工具方法

---
+++ app/src/main/java/com/oyz/materialtest/Max.kt
@@ -0,0 +1,30 @@
+package com.example.materialtest
+
+import java.lang.RuntimeException
+
+fun main() {
+    val a = 10
+    val b = 15
+    val c = 5
+    val largest = max(a, b, c)
+    println(largest)
+}
+
+fun max(vararg nums:Int):Int{
+    var maxNum = Int.MIN_VALUE
+    for (num in nums) {
+        maxNum = max(maxNum,num)
+    }
+    return maxNum
+}
+
+fun <T : Comparable<T>> max(vararg nums: T): T {
+    if (nums.isEmpty()) throw RuntimeException("Params can not be empty.")
+    var maxNum = nums[0]
+    for (num in nums) {
+        if (num > maxNum) {
+            maxNum = num
+        }
+    }
+    return maxNum
+}
