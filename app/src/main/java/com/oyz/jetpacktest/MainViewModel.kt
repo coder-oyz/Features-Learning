4dd1f058ea0aa3a7d634a93d314012e36d6f050e
coder-oyz <1583807155@qq.com>
2021-01-26 09:25:12 +0800

    ViewModel基本使用

---
+++ app/src/main/java/com/oyz/jetpacktest/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.oyz.jetpacktest
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * 通过构造函数传入参数
+ */
+//class MainViewModel(countReserved: Int) : ViewModel() {
+//    var counter = countReserved
+//}
+
+class MainViewModel() : ViewModel() {
+    var counter = 0
+}
