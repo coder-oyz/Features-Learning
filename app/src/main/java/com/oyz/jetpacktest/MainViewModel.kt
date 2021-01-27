b0ecd3c8346e39937d670c91b1e7f3cefef08552
coder-oyz <1583807155@qq.com>
2021-01-27 09:05:21 +0800

    LiveData优化用法

--- app/src/main/java/com/oyz/jetpacktest/MainViewModel.kt
+++ app/src/main/java/com/oyz/jetpacktest/MainViewModel.kt
@@ -1,12 +1,37 @@
 package com.oyz.jetpacktest
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 /**
- * 用LiveData包装counter
+ * 将原来的counter改为_counter，再加一个private修饰符，使对外不可见
+ * 再加一个counter声明为不可变的LiveData,它的get属性方法返回_counter
  */
 class MainViewModel(countReserved: Int) : ViewModel() {
+    val counter: LiveData<Int> get() =_counter
+    val _counter = MutableLiveData<Int>()
+
+    init {
+        _counter.value = countReserved
+    }
+
+    fun plusOne() {
+        val count = _counter.value ?:0
+        _counter.value = count + 1
+    }
+
+    fun clear() {
+        _counter.value = 0
+    }
+}
+
+
+/**
+ * 用LiveData包装counter（MutableLiveData 可变的数据）
+ * 但是这样暴露数据给ViewModel外面，是的外面也可以对数据进行设值
+ */
+/*class MainViewModel(countReserved: Int) : ViewModel() {
     var counter = MutableLiveData<Int>()
 
     init {
@@ -21,7 +46,7 @@ class MainViewModel(countReserved: Int) : ViewModel() {
     fun clear() {
         counter.value = 0
     }
-}
+}*/
 
 /**
  * 通过构造函数传入参数
