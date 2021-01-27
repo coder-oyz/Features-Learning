db4cf6e3ec17b6f9ede46cf4a2bb9321d33c0bdf
coder-oyz <1583807155@qq.com>
2021-01-27 10:00:47 +0800

    Room初步学习使用

--- app/src/main/java/com/oyz/jetpacktest/MainActivity.kt
+++ app/src/main/java/com/oyz/jetpacktest/MainActivity.kt
@@ -5,9 +5,11 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings.Global.putInt
+import android.util.Log
 import androidx.core.content.edit
 import androidx.lifecycle.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,6 +52,41 @@ class MainActivity : AppCompatActivity() {
         viewModel.user.observe(this) {user ->
             infoText.text = user.firstName
         }
+
+        val user1 = User("Tom", "Brady", 40)
+        val user2 = User("Tom", "Hanks", 63)
+        val userDao = AppDatabase.getDatabase(this).userDao()
+
+
+
+        addDataBtn.setOnClickListener {
+            thread {
+                user1.id = userDao.insertUser(user1)
+                user2.id = userDao.insertUser(user2)
+            }
+        }
+        updateDataBtn.setOnClickListener {
+            thread {
+                user1.age = 42
+                userDao.updateUser(user1)
+            }
+        }
+        deleteDataBtn.setOnClickListener {
+            thread {
+                userDao.deleteUserByLastName("Hanks")
+            }
+        }
+        queryDataBtn.setOnClickListener {
+            thread {
+                for (user in userDao.loadAllUsers()) {
+                    Log.d("MainActivity", user.toString())
+                }
+            }
+        }
+        /*doWorkBtn.setOnClickListener {
+            val request = OneTimeWorkRequest.Builder(SimpleWorker::class.java).build()
+            WorkManager.getInstance(this).enqueue(request)
+        }*/
 
         refreshCounter()
 
--- app/src/main/java/com/oyz/jetpacktest/User.kt
+++ app/src/main/java/com/oyz/jetpacktest/User.kt
@@ -1,4 +1,14 @@
 package com.oyz.jetpacktest
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * 定义实体
+ */
+@Entity
 data class User(var firstName: String, var lastName: String, var age: Int) {
+    //主键，自动生成
+    @PrimaryKey(autoGenerate = true)
+    var id: Long =0
 }
